a483be5df4715f8f2fdcc4f6d28006b2e838d03a
Lee <lixinghua2010@gmail.com>
2014-07-23 16:25:35 +0800

    更新了 console 的示例程序。

--- Console/console.example.js
+++ Console/console.example.js
@@ -17,6 +17,7 @@
 
 /**
  * console.log([data],[...])
+ * 向 stdout 打印并新起一行。这个函数可以像 printf() 那样接受多个参数。
  */
 console.log("这个是基本的用法，向标准输出设备打印这句话，并另起一行。");
 console.log("可以传入多个参数","，","并一起打印出来","。");
@@ -26,5 +27,60 @@ console.log("可以像'printf()'那样接受多个参数，比如 count %d", cou
 
 /**
  * console.info([data],[...])
- * 用法与console.log相同
+ * 用法与 console.log 相同。
  */
+
+/**
+ * console.error([data],[...])
+ * 同 console.log，但输出到 stderr。
+ */
+
+/**
+ * console.warn([data],[...])
+ * 用法与 console.error 相同。
+ */
+
+/**
+ * console.dir(obj)
+ * 对 obj 使用 util.inspect 并将结果字符串输出到 stdout。这个函数会忽略 obj 上的
+ * 任何自定义 inspect()。
+ *
+ * util.inspect 用于返回一个对象的字符串表现形式, 在代码调试的时候非常有用。
+ */
+
+//下面的例子用 console.dir 分别输出 console 对象和 process 对象的字符串表现形式。
+console.dir(console);
+console.dir(process);
+
+/**
+ * console.time(label)
+ * 标记一个时间点。
+ *
+ * console.timeEnd(label)
+ * 结束计时器，记录输出。
+ */
+
+//下面的例子标记了一段代码的开始的时间，并在代码结束后输出其执行所消耗的时间。
+console.time('循环100万次：');
+for (var i = 0; i < 1000000; i++) {
+  ;
+}
+console.timeEnd('循环100万次：');
+
+/**
+ * console.trace(label)
+ * 向标准错误流输出当前的调用栈。 
+ */
+
+//下面的例子将向标准错误流输出 console.trace() 函数当前的调用栈。
+console.trace();
+
+/**
+ * console.assert(expression, [message])
+ * 同 assert.ok() 函数一样，如果 expression 参数验证结果为false，则会抛出一个
+ * AssertionError 异常，异常信息为 message 参数。
+ *
+ * 貌似对中文不友好 :(
+ */
+
+console.assert(false, "an AssertionError from console.assert()");
